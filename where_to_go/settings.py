14d968e5dcbbbee08b42f5fed3ce3334cc4bbffe
Roman Okatev <43115066+Nebulawalker@users.noreply.github.com>
2023-09-23 16:09:11 +0300

    Fix bug with static files

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -114,11 +114,11 @@ USE_TZ = env.bool('USE_TZ', True)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-STATIC_URL = env.str('STATIC_URL', '/static/')
+STATIC_URL = env.str('STATIC_URL', 'static/')
 STATIC_ROOT = env.str('STATIC_ROOT', 'static/')
 
 MEDIA_ROOT = env.str('MEDIA_ROOT', 'media/')
-MEDIA_URL = env.str('MEDIA_URL', '/media/')
+MEDIA_URL = env.str('MEDIA_URL', 'media/')
 
 
 # Default primary key field type
